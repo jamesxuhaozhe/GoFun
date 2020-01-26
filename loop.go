d827987e080eb0c559002a5608d7edda42cdf2dd
James Xu <jamesxuhaozhe@gmail.com>
2020-01-26 23:30:00 +0800

    2-5 loop

---
+++ loop.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func convertToBin(n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	fmt.Println(
+		convertToBin(5),
+		convertToBin(13),
+		convertToBin(723),
+	)
+
+	printFile("abc.txt")
+	forever()
+}
+
+func forever() {
+	for {
+		fmt.Println("abc")
+	}
+}
