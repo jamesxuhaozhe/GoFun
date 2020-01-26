4d6c5e32f6fc29242ab037a3b6341405aef349e7
James Xu <jamesxuhaozhe@gmail.com>
2020-01-26 23:18:57 +0800

    2-4 branch if switch condition

---
+++ branch.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	const filename = "abc.txt"
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", contents)
+	}
+	anotherWayOfIfBranch()
+
+	fmt.Println(
+		grade(0),
+		grade(59),
+		grade(60),
+		grade(82),
+		grade(99),
+		grade(100),
+		grade(-3))
+}
+
+func anotherWayOfIfBranch() {
+	const filename = "abc.txt"
+	if contents, err := ioutil.ReadFile(filename); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", contents)
+	}
+}
+
+func grade(score int) string {
+	g := ""
+	switch {
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("Wrong score: %d", score))
+	case score < 60:
+		g = "F"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score <= 100:
+		g = "A"
+	}
+	return g
+}
