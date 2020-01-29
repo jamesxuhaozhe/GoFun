4d1bf590e1dd007ada51b5d7189b06a5c1433209
James Xu <jamesxuhaozhe@gmail.com>
2020-01-29 22:45:30 +0800

    8-1 defer example

---
+++ errorhandling/defer/defer.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"haozhexu.com/gofun/learngo/functional/fib"
+)
+
+func tryDefer() {
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	fmt.Println(3)
+	fmt.Println(4)
+}
+
+func writeFile(filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	f := fib.Fibonacci()
+	for i := 0; i < 20; i++ {
+		fmt.Fprintln(writer, f())
+	}
+}
+
+func main() {
+	//tryDefer()
+	writeFile("fib.txt")
+}
