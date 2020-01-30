186d0f0e13b1fca80a15d7ba43b0fdc832ac7bbe
James Xu <jamesxuhaozhe@gmail.com>
2020-01-30 15:33:33 +0800

    8-5 testing

--- errorhandling/filelistingserver/web.go
+++ errorhandling/filelistingserver/web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
@@ -11,8 +12,22 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
+
 		err := handler(writer, request)
+
 		if err != nil {
+
+			if userError, ok := err.(userError); ok {
+				http.Error(writer, userError.Message(), http.StatusBadRequest)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -25,6 +40,11 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 			http.Error(writer, http.StatusText(code), code)
 		}
 	}
+}
+
+type userError interface {
+	error
+	Message() string
 }
 
 func main() {
