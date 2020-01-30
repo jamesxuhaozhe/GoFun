23c5618c0bb2f34e7f3705057bf363209160893d
James Xu <jamesxuhaozhe@gmail.com>
2020-01-30 16:06:59 +0800

    9-1 testing

--- basic/basic/basic.go
+++ basic/basic/basic.go
@@ -49,9 +49,13 @@ func main() {
 
 func triangle() {
 	a, b := 3, 4
+	fmt.Println(calcTriangle(a, b))
+}
+
+func calcTriangle(a, b int) int {
 	var c int
 	c = int(math.Sqrt(float64(a*a + b*b)))
-	fmt.Println(c)
+	return c
 }
 
 func euler() {
