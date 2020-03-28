1f0d7c0fbcf2f8964cb052352b86857d79806ded
James Xu <jamesxuhaozhe@gmail.com>
2020-03-28 22:37:13 +0800

    channel restriction

--- channel/channel.go
+++ channel/channel.go
@@ -5,17 +5,20 @@ import (
 	"time"
 )
 
-func work(n int, c chan int) {
-	for {
-		fmt.Printf("Worker %d received %c\n", n, <-c)
-	}
+func createWorker(n int) chan<- int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Printf("Worker %d received %c\n", n, <-c)
+		}
+	}()
+	return c
 }
 
 func chanDemo() {
-	var channels [10]chan int
+	var channels [10]chan<- int
 	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go work(i, channels[i])
+		channels[i] = createWorker(i)
 	}
 
 	for i := 0; i < 10; i++ {
@@ -25,7 +28,6 @@ func chanDemo() {
 	for i := 0; i < 10; i++ {
 		channels[i] <- 'A' + i
 	}
-
 
 	time.Sleep(time.Millisecond)
 }
