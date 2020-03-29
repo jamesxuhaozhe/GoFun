d02d4da8f703c4ef62a6c58db5d66f5aed10d37b
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-03-29 16:17:39 +0800

    Add channel demo

--- channel/channel.go
+++ channel/channel.go
@@ -5,13 +5,16 @@ import (
 	"time"
 )
 
+func worker(id int, c chan int) {
+	for n := range c {
+
+		fmt.Printf("Worker %d received %d\n", id, n)
+	}
+}
+
 func createWorker(n int) chan<- int {
 	c := make(chan int)
-	go func() {
-		for {
-			fmt.Printf("Worker %d received %c\n", n, <-c)
-		}
-	}()
+	go worker(n, c)
 	return c
 }
 
@@ -34,13 +37,31 @@ func chanDemo() {
 
 func bufferedChannel() {
 	c := make(chan int, 3)
-	c <- 1
-	c <- 2
-	c <- 3
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	time.Sleep(time.Millisecond)
 
 }
 
+func channelClose() {
+	c := make(chan int)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	close(c)
+	time.Sleep(time.Millisecond)
+}
+
 func main() {
-	//chanDemo()
+	fmt.Println("Channel as first class citizen")
+	chanDemo()
+	fmt.Println("Buffered channel")
 	bufferedChannel()
+	fmt.Println("Channel close and range")
+	channelClose()
 }
