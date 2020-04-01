eda45451d7d0af86cc2d2b9238ea5d6fc16b0759
James Xu <jamesxuhaozhe@gmail.com>
2020-04-01 21:21:15 +0800

    finished fetcher

---
+++ crawler/fetcher/fetcher.go
@@ -0,0 +1,40 @@
+package fetcher
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+
+	"golang.org/x/text/transform"
+)
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("wrong status code: %d", resp.StatusCode)
+	}
+
+	e := determineEncoding(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func determineEncoding(r io.ReadCloser) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		log.Printf("Fetcher error: %v", err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
