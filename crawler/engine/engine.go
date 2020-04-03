8e70f3f2031882459e469a24b383f3b3f969283c
James Xu <jamesxuhaozhe@gmail.com>
2020-04-03 10:22:59 +0800

    refactor simple engine

--- crawler/engine/engine.go
+++ crawler/engine/engine.go
@@ -16,17 +16,26 @@ func Run(seeds ...Request) {
 		r := requests[0]
 		requests = requests[1:]
 
-		log.Printf("Fetching %s", r.Url)
-		body, err := fetcher.Fetch(r.Url)
+		parseResult, err := worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error "+"fetching url %s: %v", r.Url, err)
+			continue
 		}
 
-		parseResult := r.ParseFunc(body)
 		requests = append(requests, parseResult.Requests...)
 
 		for _, item := range parseResult.Items {
 			log.Printf("Got item %v", item)
 		}
 	}
+}
+
+func worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching %s", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error "+"fetching url %s: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+
+	return r.ParseFunc(body), nil
 }
