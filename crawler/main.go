05845f70f59c47824df0727011d6ac914ff559eb
James Xu <jamesxuhaozhe@gmail.com>
2020-04-03 15:28:53 +0800

    done with concurrent engine

--- crawler/main.go
+++ crawler/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"haozhexu.com/gofun/learngo/crawler/engine"
+	"haozhexu.com/gofun/learngo/crawler/scheduler"
 	"haozhexu.com/gofun/learngo/crawler/zhenai/parser"
 )
 
 func main() {
-	engine.Run(engine.Request{
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.SimpleScheduler{},
+		WorkerCount: 10,
+	}
+	e.Run(engine.Request{
 		Url:       "http://www.zhenai.com/zhenghun",
 		ParseFunc: parser.ParseCityList,
 	})
-
 }
