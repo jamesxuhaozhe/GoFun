b6ab9bb5891ff472a3383d31787641e11737fad8
James Xu <jamesxuhaozhe@gmail.com>
2020-04-06 22:46:59 +0800

    implement the queuedScheduler

--- crawler/scheduler/Queued.go
+++ crawler/scheduler/Queued.go
@@ -1,4 +1,48 @@
 package scheduler
 
+import "haozhexu.com/gofun/learngo/crawler/engine"
+
 type QueuedScheduler struct {
+	requestChan chan engine.Request
+	workerChan  chan chan engine.Request
+}
+
+func (q *QueuedScheduler) Submit(r engine.Request) {
+	q.requestChan <- r
+}
+
+func (q *QueuedScheduler) WorkerReady(w chan engine.Request) {
+	q.workerChan <- w
+}
+
+func (q *QueuedScheduler) ConfigureMasterWorkerChan(chan engine.Request) {
+	panic("implement me")
+}
+
+func (q *QueuedScheduler) Run() {
+	q.workerChan = make(chan chan engine.Request)
+	q.requestChan = make(chan engine.Request)
+	go func() {
+		var requestQ []engine.Request
+		var workerQ []chan engine.Request
+		for {
+			var activeRequest engine.Request
+			var activeWorker chan engine.Request
+			if len(requestQ) > 0 && len(workerQ) > 0 {
+				activeWorker = workerQ[0]
+				activeRequest = requestQ[0]
+			}
+			select {
+			case r := <-q.requestChan:
+				// send r to a ?worker
+				requestQ = append(requestQ, r)
+			case w := <-q.workerChan:
+				// send ?next_request to w
+				workerQ = append(workerQ, w)
+			case activeWorker <- activeRequest:
+				workerQ = workerQ[1:]
+				requestQ = requestQ[1:]
+			}
+		}
+	}()
 }
